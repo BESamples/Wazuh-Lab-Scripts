d0ab2558def1237030f658a047d8e1e69f7bcb2f
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-05-19 12:19:34 -0500

    Update Yara-test.ps1

--- Customer Yara Rules/Yara-test.ps1
+++ Customer Yara Rules/Yara-test.ps1
@@ -10,3 +10,15 @@ rule Test_Malware_String
 '@
 
 & "C:\Program Files (x86)\ossec-agent\active-response\bin\yara\yara64.exe" -s "C:\Program Files (x86)\ossec-agent\active-response\bin\yara\rules\test-malware.yar" "C:\Wazuh-Test\evil.txt"
+
+
+Set-Content -Path "C:\Program Files (x86)\ossec-agent\active-response\bin\yara\rules\always-match.yar" -Value @'
+rule Always_Match
+{
+    condition:
+        true
+}
+'@
+
+
+& "C:\Program Files (x86)\ossec-agent\active-response\bin\yara\yara64.exe" "C:\Program Files (x86)\ossec-agent\active-response\bin\yara\rules\always-match.yar" "C:\Wazuh-Test\evil.txt"
