fbbad368c5b9b8ecdbea73bd66392795156f766d
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-05-19 12:08:49 -0500

    Create Yara-test.ps1

---
+++ Customer Yara Rules/Yara-test.ps1
@@ -0,0 +1,1 @@
+& "C:\Program Files (x86)\ossec-agent\active-response\bin\yara\yara64.exe" "C:\Program Files (x86)\ossec-agent\active-response\bin\yara\rules\test-malware.yar" "C:\Wazuh-Test\evil.txt"
