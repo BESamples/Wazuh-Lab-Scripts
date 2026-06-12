15d619710205c6fa710cbfbc7c3ee8a664bde481
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-06-12 16:48:02 -0500

    Update manage-wazuh-agent-gui.ps1

--- manage-wazuh-agent-gui.ps1
+++ manage-wazuh-agent-gui.ps1
@@ -940,7 +940,7 @@ function Launch-ADLabGUI {
 
 function Download-DlpLabGUI {
 
-    $Url = "https://raw.githubusercontent.com/BESamples/Wazuh-Lab-Scripts/main/Powershell-Scripts/windows-dlp-lab-gui.ps1"
+    $Url = "https://raw.githubusercontent.com/BESamples/Wazuh-Lab-Scripts/main/windows-dlp/windows-dlp-lab-gui.ps1"
     $Destination = "$PSScriptRoot\windows-dlp-lab-gui.ps1"
 
     if (Test-Path $Destination) {
