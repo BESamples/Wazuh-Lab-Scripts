52a0c1fcc05c1b3f8b8fd56524db8c3472835ded
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-06-12 15:24:23 -0500

    Update dlp_test.ps1

--- windows-dlp/dlp_test.ps1
+++ windows-dlp/dlp_test.ps1
@@ -2,3 +2,4 @@ cd C:\
 git clone https://github.com/YOUR-GITHUB-USERNAME/Wazuh-Lab-Scripts.git
 cd C:\Wazuh-Lab-Scripts\windows-dlp
 powershell.exe -ExecutionPolicy Bypass -File .\setup_windows_dlp.ps1
+powershell.exe -ExecutionPolicy Bypass -File C:\ProgramData\DLP\run_dlp_once.ps1
