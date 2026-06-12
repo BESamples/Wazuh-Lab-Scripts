2893f726db71d38ae5e8b38a106e52d1a7b28ae2
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-06-12 15:24:07 -0500

    Create dlp_test.ps1

---
+++ windows-dlp/dlp_test.ps1
@@ -0,0 +1,4 @@
+cd C:\
+git clone https://github.com/YOUR-GITHUB-USERNAME/Wazuh-Lab-Scripts.git
+cd C:\Wazuh-Lab-Scripts\windows-dlp
+powershell.exe -ExecutionPolicy Bypass -File .\setup_windows_dlp.ps1
