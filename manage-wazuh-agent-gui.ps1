6b0e025f404695f996a0df3aa2cacba8603dde06
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-06-17 16:39:10 -0500

    Update manage-wazuh-agent-gui.ps1

--- manage-wazuh-agent-gui.ps1
+++ manage-wazuh-agent-gui.ps1
@@ -949,7 +949,7 @@ function Launch-ADLabGUI {
 function Download-DlpLabGUI {
 
     $Url = "https://github.com/BESamples/Wazuh-Lab-Scripts/blob/main/Powershell-Scripts/setup-fsrm-dlp-lab.ps1"
-    $Destination = "$PSScriptRoot\windows-dlp-lab-gui.ps1"
+    $Destination = "$PSScriptRoot\setup-fsrm-dlp-lab.ps1"
 
     if (Test-Path $Destination) {
         $Overwrite = [System.Windows.Forms.MessageBox]::Show(
