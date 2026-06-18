337ebbba3bd9ea5c60a9a55ce934f4cc18fe3024
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-06-18 12:41:47 -0500

    Update wazuh-lab-sim-gui.ps1

--- Powershell-Scripts/wazuh-lab-sim-gui.ps1
+++ Powershell-Scripts/wazuh-lab-sim-gui.ps1
@@ -400,7 +400,7 @@ function New-GuiButton {
     $Form.Controls.Add($Button)
 }
 
-New-GuiButton "Generate Random PII" 380 300 { Generate-Random-PII }
+New-GuiButton "Generate Random PII" 380 150 { Generate-Random-PII }
 New-GuiButton "Create Custom PII File" 25 410 { Create-Custom-PII }
 
 New-GuiButton "Create Realistic Business File" 25 245 { Create-Realistic-Business-PII }
