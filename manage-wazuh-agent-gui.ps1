f60f874f0ed4e82ef23594e0088d9f91dd7d724f
Brian Samples <146466323+BESamples@users.noreply.github.com>
2026-06-18 09:41:03 -0500

    Update manage-wazuh-agent-gui.ps1

--- manage-wazuh-agent-gui.ps1
+++ manage-wazuh-agent-gui.ps1
@@ -948,8 +948,8 @@ function Launch-ADLabGUI {
 
 function Download-DlpLabGUI {
 
-    $Url = "https://raw.githubusercontent.com/BESamples/Wazuh-Lab-Scripts/main/Powershell-Scripts/windows-dlp-lab-gui.ps1"
-    $Destination = "$PSScriptRoot\windows-dlp-lab-gui.ps1"
+   $Url = "https://raw.githubusercontent.com/BESamples/Wazuh-Lab-Scripts/main/windows-dlp/windows_dlp-gui.ps1"
+$Destination = "$PSScriptRoot\windows-dlp-lab-gui.ps1"
 
     if (Test-Path $Destination) {
         $Overwrite = [System.Windows.Forms.MessageBox]::Show(
@@ -1009,8 +1009,8 @@ function Test-IsWindowsServer2019 {
 
 function Download-FsrmDlpLab {
 
-    $Url = "https://raw.githubusercontent.com/BESamples/Wazuh-Lab-Scripts/main/Powershell-Scripts/fsrm-dlp-lab-setup.ps1"
-    $Destination = "$PSScriptRoot\fsrm-dlp-lab-setup.ps1"
+    $Url = "https://raw.githubusercontent.com/BESamples/Wazuh-Lab-Scripts/main/Powershell-Scripts/setup-fsrm-dlp-lab.ps1"
+    $Destination = "$PSScriptRoot\setup-fsrm-dlp-lab.ps1"
 
     if (Test-Path $Destination) {
         $Overwrite = [System.Windows.Forms.MessageBox]::Show(
